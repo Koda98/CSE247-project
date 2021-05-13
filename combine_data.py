0180997cf54f62856abd1a442989a8e1af91c24c
Kyle Oda <1624udon@gmail.com>
2021-05-12 23:04:50 -0700

    updated combine_data

--- combine_data.py
+++ combine_data.py
@@ -20,27 +20,8 @@ def combine_data(path, filename):
 
 
 if __name__ == "__main__":
+    combine_data("Data/GeneratedData/CleanData_IDD/Music", "IDD_music")
     combine_data("Data/GeneratedData/CleanData_IDD/Rest", "IDD_rest")
-
-# X = []
-# Y = []
-#
-# data_path = "Data/GeneratedData/CleanData_IDD/"
-# base_path = os.getcwd()
-#
-# for type_dir in os.listdir(data_path):
-#     if type_dir[0] == ".":
-#         continue
-#     type_dir_path = os.path.join(base_path, data_path, type_dir)
-#     for subject in os.listdir(type_dir_path):
-#         if subject[0] == ".":
-#             continue
-#         subject_dir_path = os.path.join(type_dir_path, subject)
-#         for file in os.listdir(subject_dir_path):
-#             if "Music" in file:
-#                 Y.append(1)
-#             elif "Rest" in file:
-#                 Y.append(0)
-#             X.append(np.loadtxt(os.path.join(type_dir_path, subject, file)))
-#         print(f"Loaded {subject} files")
+    combine_data("Data/GeneratedData/CleanData_TDC/Music", "TDC_music")
+    combine_data("Data/GeneratedData/CleanData_TDC/Rest", "TDC_rest")
 
